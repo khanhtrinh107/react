2311b6fd620a371f0deb393298578f6e7c888658
khanhtrinh107 <khanhtrinh107@gmail.com>
2023-03-18 18:03:03 +0700

    ok

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,41 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DisplayInfor.scss'
 import logo from './../logo.svg'
-class DisplayInfor extends React.Component{
-    state = {
-        isShowListUser : true
+const DisplayInfor = (props) => {
+    let listUser = props.listUser
+    const [isShowOrHide,setIsShowOrHide] = useState(true)
+    const change = () => {
+        setIsShowOrHide(!isShowOrHide)
     }
-    handleShowOrHide = () =>{
-        this.setState({
-            isShowListUser : !this.state.isShowListUser
-        })
-    }
-    handleDelete = (userId) => {
-        this.props.handleDelete(userId)
-    }
-    render(){
-        const listUser = this.props.listUser
-        return (
-           <div className='parents' >
-                <img src={logo} />
-                <div className='button' >
-                    <button onClick={() => {this.handleShowOrHide()}}>
-                        {this.state.isShowListUser === true ? 'Hide List User' : 'Show List User'}
-                    </button>
-                </div>
-                {this.state.isShowListUser && <div>
-                    {listUser.map((user) => {
-                        return (
-                            <div className='child' key={user.id} >
-                                <div>name {user.name} , age : {user.age}</div>
-                                <button onClick={() => this.handleDelete(user.id)}>Delete</button>
-                            </div>    
-                        )
-                    })}
-                </div>}
-           </div>
-        )
-    }
+    return (
+        <div className='parents' >
+             <img src={logo} />
+             <button onClick={() => change()} >
+                {isShowOrHide ? 'Hide List User' : 'Show List User'}
+             </button>
+             {isShowOrHide && <div>
+                 {listUser.map((user) => {
+                     return (
+                         <div className='child' key={user.id} >
+                             <div>name {user.name} , age : {user.age}</div>
+                             <button onClick={() => props.handleDelete(user.id)}>Delete</button>
+                         </div>    
+                     )
+                 })}
+             </div>}
+        </div>
+     )
 }
 
 export default DisplayInfor
